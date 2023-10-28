45ae0cab629a367e1bc9ed07cf4afe4bd5b105e4
tom hikari <tomprescott1565@gmail.com>
2023-10-28 13:44:00 +0100

    logout

--- app/GraphQL/Mutations/Logout.php
+++ app/GraphQL/Mutations/Logout.php
@@ -1,12 +1,26 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace App\GraphQL\Mutations;
+
+use App\Models\Session;
 
 final readonly class Logout
 {
     /** @param  array{}  $args */
     public function __invoke(null $_, array $args)
     {
-        // TODO implement the resolver
+        $user = auth()->user();
+
+        if ($user) {
+            $login = new Session();
+            $login->user_id = $user->id;
+            $login->action = "LOGOUT";
+            $login->save();
+        }
+
+        auth()->logout();
+        return ["message" => "Logout success", "status" => 200];
     }
 }
