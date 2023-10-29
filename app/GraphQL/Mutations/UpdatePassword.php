f3c8ca523893ff6778533ddd7988e70244924ce0
tom hikari <tomprescott1565@gmail.com>
2023-10-29 16:29:54 +0100

    updatePassword

---
+++ app/GraphQL/Mutations/UpdatePassword.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use App\Helpers\Password;
+use App\Models\User;
+
+final readonly class UpdatePassword
+{
+    /** @param  array{}  $args */
+    public function __invoke(null $_, array $args)
+    {
+        if (!auth()->user()) {
+            return ["message" => "You are not logged in", "status" => 403];
+        }
+
+        $user = User::find(auth()->user()->id);
+
+        $login = auth()->attempt(['email' => $user->email, 'password' => $args['password']]);
+
+        if (!$login) {
+            return ["message" => "Password not match", "status" => 403];
+        }
+
+        $check_password = Password::check($args['new_password'], $args['password_confirmation']);
+
+        if (!$check_password['ok']) {
+            return ["message" => $check_password['message'], "status" => 403];
+        }
+
+        $user->password = bcrypt($args['new_password']);
+
+        $user->save();
+
+        return ["message" => "Password updated", "status" => 200];
+    }
+}
---
+++ app/Http/Controllers/UploadImage.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UploadImage extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        request()->validate([
+            'image'=> 'required|image|mimes:jpeg,png,jpg,webp|max:1000',
+        ]);
+
+        $imageName = time() . '.' . $request->image->extension();
+        request()->image->move(public_path('storage'), $imageName);
+        return response()->json($imageName, 200);
+    }
+}
