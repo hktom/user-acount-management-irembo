47d66239c284f65ebb805510f7a33b47149f9c72
tom hikari <tomprescott1565@gmail.com>
2023-10-29 16:04:52 +0100

    remove boilerprate code

--- app/GraphQL/Mutations/UpdateProfile.php
+++ app/GraphQL/Mutations/UpdateProfile.php
@@ -17,36 +17,15 @@ final readonly class UpdateProfile
             return ["message" => "User not found", "status" => 403];
         }
 
-        if (isset($args['first_name'])) {
-            $user->first_name = $args['first_name'];
-        }
+        foreach ($args as $key => $value) {
 
-        if (isset($args['last_name'])) {
-            $user->last_name = $args['last_name'];
-        }
-
-        if (isset($args['photo'])) {
-            $user->last_name = $args['photo'];
-        }
-
-        if (isset($args['nationality'])) {
-            $user->nationality = $args['nationality'];
-        }
-
-        if (isset($args['gender'])) {
-            $user->gender = $args['gender'];
-        }
-
-        if (isset($args['date_of_birth'])) {
-            $user->date_of_birth = $args['date_of_birth'];
-        }
-
-        if (isset($args['marital_status'])) {
-            $user->marital_status = $args['marital_status'];
+            if(isset($args[$key]) && $args[$key] != "undefined" && $key != "nationality") {
+                $user->$key = $args[$key];
+            }
         }
 
         $user->save();
 
-        return ["message" => "Profile has been updated", "status" => 200, 'data' => $user];
+        return ["message" => "Profile has been updated", "status" => 200, 'user' => $user];
     }
 }
