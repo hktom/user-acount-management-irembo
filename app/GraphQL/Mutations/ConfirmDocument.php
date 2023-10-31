895f9439ddb8c9677f2f480201f9968997c415a9
tom hikari <tomprescott1565@gmail.com>
2023-10-31 07:27:16 +0100

    verification profiles

--- app/GraphQL/Mutations/ConfirmDocument.php
+++ app/GraphQL/Mutations/ConfirmDocument.php
@@ -1,8 +1,11 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace App\GraphQL\Mutations;
 
 use App\Models\User;
+use App\Helpers\Email;
 
 final readonly class ConfirmDocument
 {
@@ -16,20 +19,36 @@ final readonly class ConfirmDocument
         }
 
         $user = User::find($args['user_id']);
-        
+
         if (!$user) {
             return ["message" => "User not found", "status" => 403];
         }
 
         $document = $user->document;
-        $document->confirmed_at = now();
+        if ($args['status'] == 'VERIFIED') {
+            $document->verified_at = now();
+        }
+        $user->status = $args['status'];
         $document->save();
-
-        $user->status = 'VERIFIED';
         $user->save();
 
-        // send email
+        $message = [
+            'VERIFIED' => "We are happy to inform you that your document has been verified. You can now proceed to use our services. Thank you for choosing Z.",
+            'UNVERIFIED' => "We are sorry to inform you that your document has been rejected. Please contact our support team for more information. Thank you for choosing Z."
+        ];
 
-        return ["message" => "Document has been confirmed", "status" => 200, 'data' => $user];
+        // send email
+        Email::sender($user->email, [
+            'subject' => "Official document confirmation update",
+            'title' => "Official document confirmation update",
+            'content' => $message[$args['status']],
+            'btn_label' => "Go to Z",
+            'btn_url' => env('CLIENT_URL'),
+            'footer' => "With love from Z"
+        ]);
+
+        $users = User::all();
+
+        return ["message" => "Document has been confirmed", "status" => 200, 'users' => $users];
     }
 }
